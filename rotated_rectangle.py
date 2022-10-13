8838f40669d923085191246636d35d778d36452f
TheTumbleweedKid <50089697+TheTumbleweedKid@users.noreply.github.com>
2022-10-13 13:19:04 +1300

    Created the RotatedRectangle class.
    Note, math utility functions have been put in a separate file for convenience. Also, the class needs testing (I'm not sure if I actually need to change anything for it to work with pygame's weird coordinate-system), and I'd like to add (convex) Polygons too, since they operate similarly (are really the general form of rotated rectangles).

---
+++ rotated_rectangle.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 13 11:53:47 2022
+
+@author: Tumbl
+"""
+
+from numpy import sqrt, pi, sin, cos, arctan
+
+from util import pointLineSide, distanceSegmentPoint2
+
+class RotatedRectangle:
+    """
+    Object for storing a rotatable rectangle.
+    Assumes a coordinate system wherein the positive x-axis faces to the left,
+    while the positive y-axis faces down (as used in pygame).
+    """
+
+    def __init__(self, x, y, length, thickness):
+        """
+        Creates a new RotatedRectangle centred on a given point,
+        with a rotation of zero ('length' going along the x-axis,
+        'thickness' going along the y-axis), initialising the corner-points too.
+
+        Parameters
+        ----------
+        x : float
+            The horizontal centre of the rectangle.
+        y : TYPE
+            The vertical centre of the rectangle (increasing y moving downwards).
+        length : float
+            The length of the rectangle (dimension parallel to its axis of rotation).
+        thickness : float
+            The thickness of the rectangle (dimension perpendicular to its axis of rotation).
+
+        Returns
+        -------
+        None.
+
+        """
+        # coordinates of the centre
+        self.x = x
+        self.y = y
+
+        # dimensions of the rectangle
+        self.length = length  # (horizontal width when rotation = 0)
+        self.thickness = thickness  # (vertical width when rotation = 0)
+
+        # useful for calculating where the points should be
+        self.hypotenuse = sqrt((0.5 * length) ** 2 + (0.5 * thickness) ** 2)
+        self.angleOffset = arctan(thickness / length)
+
+        # in radians, measured anticlockwise from the positive x-axis
+        self.rotation = 0
+
+        self.points = [None, None, None, None]
+
+        self.set_rotation(0)
+
+    def set_rotation(self, new_rotation):
+        """
+        Sets the rotation of the rectangle to a new rotation (measured in radians, anticlockwise from the positive x-axis).
+
+        Parameters
+        ----------
+        new_rotation : float
+            The angle in radians anticlockwise from the positive x-axis to the new rotation angle.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.rotation = new_rotation
+
+        self.points[0] = [
+            self.x + self.hypotenuse * cos(self.rotation + self.angleOffset),
+            self.y - self.hypotenuse * sin(self.rotation + self.angleOffset)]
+        self.points[1] = [
+            self.x + self.hypotenuse * cos(self.rotation - self.angleOffset),
+            self.y - self.hypotenuse * sin(self.rotation - self.angleOffset)]
+        self.points[2] = [
+            self.x + self.hypotenuse * cos(self.rotation + pi + self.angleOffset),
+            self.y - self.hypotenuse * sin(self.rotation + pi + self.angleOffset)]
+        self.points[3] = [
+            self.x + self.hypotenuse * cos(self.rotation + pi - self.angleOffset),
+            self.y - self.hypotenuse * sin(self.rotation + pi - self.angleOffset)]
+    
+    def set_position(self, new_x, new_y):
+        """
+        Re-centers the rectangle on a given point, translating all boundary points along with it.
+
+        Parameters
+        ----------
+        new_x : float
+            The x-coordinate of the new centrepoint.
+        new_y : float
+            The y-coordinate of the new centrepoint.
+
+        Returns
+        -------
+        None.
+
+        """
+        delta_x = new_x - self.x
+        delta_y = new_y - self.y
+        
+        for i in range(4):
+            self.points[i][0] += delta_x
+            self.points[i][1] += delta_y
+            
+    def contains_point(self, point):
+        """
+        Check whether a point is inside the rectangle.
+
+        Parameters
+        ----------
+        point : float array
+            The point to test.
+            [0]: x-coordinate of the point.
+            [1]: y-coordinate of the point.
+
+        Returns
+        -------
+        bool
+            True if the point is inside the rectangle, otherwise False.
+
+        """
+        for i in range(4):
+            if i < 3:
+                if pointLineSide(self.points[i], self.points[i + 1], point) > 0:
+                    return False
+            elif pointLineSide(self.points[i], self.points[0], point) > 0:
+                return False
+        
+        return True
+    
+    def overlaps_circle(self, centre, radius):
+        """
+        Check whether there is any overlap between a circle and the rectangle.
+
+        Parameters
+        ----------
+        centre : float array
+            The centre of the circle.
+            [0]: x-coordinate of the centre.
+            [1]: y-coordinate of the centre.
+        radius : float
+            The radius of the circle.
+
+        Returns
+        -------
+        bool
+            True if the centre of the circle is inside the rectangle
+            and/or the circle covers any of the boundary lines of the rectangle, otherwise False.
+
+        """
+        if self.contains_point(centre):
+            return True;
+        
+        for i in range(4):
+            if i < 3:
+                if distanceSegmentPoint2(self.points[i], self.points[i + 1], centre) <= radius ** 2:
+                    return True
+            elif distanceSegmentPoint2(self.points[i], self.points[0], centre) <= radius ** 2:
+                return True
+        
+        return False
+    
+    def overlaps_rot_rect(self, other):
+        """
+        Check whether there is any overlap between this RotatedRectangle and another.
+        Note that it is sufficient to check whether any or the corner points of each
+        rectangle are contained within the other rectangle.
+
+        Parameters
+        ----------
+        other : RotatedRectangle
+            The rotated rectangle to test.
+
+        Returns
+        -------
+        bool
+            True if the rectangles overlap, otherwise False.
+
+        """
+        for point in other.points:
+            if self.contains_point(point):
+                return True
+        for point in self.points:
+            if other.contains_point(point):
+                return True
+        return False
+        
---
+++ util.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Utility functions for rotated-rectangle collision maths.
+Assumes a coordinate system wherein the positive x-axis faces to the left,
+while the positive y-axis faces down (as used in pygame).
+
+Created on Thu Oct 13 12:17:28 2022
+
+@author: Tumbl
+"""
+from numpy import sign, sqrt
+
+
+def pointLineSide(start, end, point):
+    """
+    Determines on which side of the given line the point is.
+    Credit to LibGDX.Intersector class.
+
+    Parameters
+    ----------
+    start : float array
+        The start of the line.
+        [0]: x-coordinate of the start.
+        [1]: y-coordinate of the start.
+    end : float array
+        The end of the line.
+        [0]: x-coordinate of the end.
+        [1]: y-coordinate of the end.
+    point : float array
+        The point to test.
+        [0]: x-coordinate of the point.
+        [1]: y-coordinate of the point.
+
+    Returns
+    -------
+    int
+        1 if the point is to the left of the line,
+        -1 if it is to the right,
+        or 0 if it is on the line.
+        Left and right are relative to the line's direction which goes from start to end.
+    
+    """
+    return sign((end[0] - start[0]) * (point[1] - start[1]) - (end[1] - start[1]) * (point[0] - start[0]))
+
+def distanceSegmentPoint2(start, end, point):
+    """
+    Finds the shortest squared distance between a point and a line segment.
+
+    Parameters
+    ----------
+    start : float array
+        The start of the line.
+        [0]: x-coordinate of the start.
+        [1]: y-coordinate of the start.
+    end : float array
+        The end of the line.
+        [0]: x-coordinate of the end.
+        [1]: y-coordinate of the end.
+    point : float array
+        The point.
+        [0]: x-coordinate of the point.
+        [1]: y-coordinate of the point.
+
+    Returns
+    -------
+    float
+        The shortest distance from the line segment to the point, squared.
+
+    """
+    length2 = (end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2
+    
+    if length2 == 0:
+        return (point[0] - start[0]) ** 2 + (point[1] - start[1]) ** 2
+    
+    t = ((point[0] - start[0]) * (end[0] - start[0]) + (point[1] - start[1]) * (end[1] - start[1])) / length2
+    
+    if t <= 0:
+        # Point is 'before' the start of the line segment, so closest to the startpoint.
+        return (point[0] - start[0]) ** 2 + (point[1] - start[1]) ** 2
+    if t >= 1:
+        # Point is 'past' the end of the line segment, so closest to the endpoint.
+        return (point[0] - end[0]) ** 2 + (point[1] - end[1]) ** 2
+    
+    nearest = [
+        start[0] + t * (end[0] - start[0]),
+        start[1] + t * (end[1] - start[1])]
+    
+    return (point[0] - nearest[0]) ** 2 + (point[1] - nearest[1]) ** 2
